5964d64de5d07c7bc0189401627b077e94b196b4
João Pedro Longo de Oliveira <102171985+jplofield@users.noreply.github.com>
2025-10-22 13:05:26 +0000

    removendo basename

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Defina o `basename` para produção e deixe vazio em desenvolvimento
-const basename = process.env.NODE_ENV === "production" ? "/projetoFindduo" : "";
-
 root.render(
   <React.StrictMode>
     <HashRouter>
